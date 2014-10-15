140dbf4b92b6061a5c9191db57a1aa5237ea7946
rahulaga <rahulaga@gmail.com>
2014-10-14 18:31:56 -0700

    adding PUT method

--- jax-rs-rest/src/main/java/com/irahul/rest/Hello.java
+++ jax-rs-rest/src/main/java/com/irahul/rest/Hello.java
@@ -16,8 +16,11 @@ package com.irahul.rest;
  * You should have received a copy of the GNU General Public License 
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -46,7 +49,18 @@ public class Hello {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String sayHelloJSON() {
+	public String sayHelloJSON(@PathParam(value="name") String name) {
+		
 		return "{\"hello\":\"Hello Code Camp\"}";
 	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/name/{name}")
+	public String sayPut(@PathParam(value="name") String name, String body ) {
+		System.out.println(body);
+		return "{\"hello\":\"Hello Code Camp+"+name+"\"}";
+	}
+	
 }
